a1ef9613d8048462eef9b30ac758082490e992f0
neutralpronoun <alexowendavies@gmail.com>
2025-03-13 15:24:24 +0000

    Another graphormer testing fix

--- fixed_train_set.py
+++ fixed_train_set.py
@@ -641,9 +641,9 @@ def evaluate_model_with_noise(model,
             # train_loader = create_dataloader_with_paths(train_data_list, batch_size=batch_size)
             for data in noisy_test_data_list:
                 data.x = torch.hstack((data.x, data.pe))
-            train_loader = DataLoader(noisy_test_data_list, batch_size=batch_size, shuffle=True)
+            noisy_test_loader = DataLoader(noisy_test_data_list, batch_size=batch_size, shuffle=True)
         else:
-            train_loader = DataLoader(noisy_test_data_list, batch_size=batch_size, shuffle=True)
+            noisy_test_loader = DataLoader(noisy_test_data_list, batch_size=batch_size, shuffle=True)
     else:
         # Use original noisy dataset without positional encodings
         if layer_type == "graphormer":
